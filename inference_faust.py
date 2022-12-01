e8d699c24da5500d5b2ca12a5b162e89f702d188
EJShim <soulrommel@gmail.com>
2022-12-01 11:57:12 +0900

    [feat] inference baseline finished

--- inference_faust.py
+++ inference_faust.py
@@ -5,6 +5,7 @@ import torch
 from utils import mesh_sampling, utils
 from utils.mesh_sampling import Mesh
 import vtk
+from vtk.util import numpy_support
 
 from models import AE, AE_single
 
@@ -40,18 +41,19 @@ device = torch.device('cpu')
 
 # load dataset
 template_fp = osp.join('template', 'smpl_male_template.ply')
+reader = vtk.vtkPLYReader()
+reader.SetFileName(template_fp)
+reader.Update()
+template_poly = reader.GetOutput()
 
 # generate/load transform matrices
 transform_fp = osp.join('data', 'transform.pkl')
 
 if not osp.exists(transform_fp):
     print('Generating transform matrices...')
-    reader = vtk.vtkPLYReader()
-    reader.SetFileName(template_fp)
-    reader.Update()
-
+    
     mesh = Mesh()
-    mesh.SetPolyData(reader.GetOutput())
+    mesh.SetPolyData(template_poly)
     
     ds_factors = args.ds_factors    
     _, A, D, U, F = mesh_sampling.generate_transform_matrices(mesh, ds_factors)
@@ -82,10 +84,7 @@ model = AE(args.in_channels,
         edge_index_list,
         down_transform_list,
         up_transform_list,
-        K=args.K)    
-
-print(model.state_dict().keys())
-
+        K=args.K)
 
 latest_checkpoint = 'work_dir/FAUST/checkpoint_0960.pt'
 data = torch.load(latest_checkpoint, map_location=device)
@@ -93,3 +92,37 @@ state_dict = data['model_state_dict']
 state_dict = {k.partition('module.')[2]: v for k,v in state_dict.items()} # Remove Prefix
 model.load_state_dict(state_dict)
 
+
+train_latent_vectors = data['train_latent_vecs']['weight']
+
+sample_idx = 0
+sample_input = train_latent_vectors[sample_idx:sample_idx+1]
+sample_output = model(sample_input)
+
+# Need to denormalize!!
+print(sample_output.shape)
+
+
+## Visualize
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+renWin = vtk.vtkRenderWindow()
+renWin.SetSize(1000, 1000)
+iren.SetRenderWindow(renWin)
+ren = vtk.vtkRenderer()
+renWin.AddRenderer(ren)
+
+output_polydata = vtk.vtkPolyData()
+output_polydata.DeepCopy(template_poly)
+output_polydata.GetPoints().SetData(numpy_support.numpy_to_vtk(sample_output[0].detach().numpy()))
+
+
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(output_polydata)
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+ren.AddActor(actor)
+ren.ResetCamera()
+renWin.Render()
+iren.Start()
